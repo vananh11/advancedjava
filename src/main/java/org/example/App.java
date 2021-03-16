2aa864d875afc61d3be2f64d5c766c0ab8eacce1
vananh11 <vananhsp2012@gmail.com>
2021-03-16 21:39:44 +0700

    Revert code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,9 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        int a =10;
-        int b =20;
-        int c = a+b;
-        System.out.println( "Hello World!" + c);
+       
     }
 }
