61c2d61cf546f2411439a429da00b015ab088343
vananh11 <vananhsp2012@gmail.com>
2021-03-16 21:38:57 +0700

    modify main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,15 @@ package org.example;
 
 /**
  * Hello world!
- *
+ *c
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int a =10;
+        int b =20;
+        int c = a+b;
+        System.out.println( "Hello World!" + c);
     }
 }
